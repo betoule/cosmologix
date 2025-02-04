83d84fc6e1ec9a53f49436106328ba9a7ca3b601
Marc Betoule <marc.betoule@lpnhe.in2p3.fr>
2025-02-04 14:40:41 +0100

    refactor distance compution

--- edriscosmo/background.py
+++ edriscosmo/background.py
@@ -1,55 +1,47 @@
 import jax.numpy as jnp
 from jax import lax
 import jax
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Dict
 from tools import linear_interpolation, Constants, restrict
 from radiation import Omega_n_mass, Omega_n_rel, Tcmb_to_Omega_gamma
 
 
-def Omega_c(params):
+def Omega_c(params: Dict[str, float]) -> float:
+    """Compute Omega_c from the total matter density minus baryonic and neutrino contributions."""
     h = params["H0"] / 100
     Omega_b0 = params['Omega_b_h2'] / h ** 2
     Omega_nu_mass = Omega_n_mass(params, jnp.atleast_1d(1.))[0]
     return params['Omega_m'] - Omega_b0 - Omega_nu_mass
 
-
-def Omega_de(params, Omega_n_rel):
+def Omega_de(params: Dict[str, float], Omega_n_rel: float) -> float:
+    """Calculate the dark energy density parameter."""
     omg = Tcmb_to_Omega_gamma(params["Tcmb"], params["H0"])
     return 1 - params['Omega_m'] - omg - Omega_n_rel - params['Omega_k']
 
+def dzoveru3H(params: Dict[str, float], u: jnp.ndarray) -> jnp.ndarray:
+    """Integrand for inverse expansion rate integrals.
 
-def dzoveru3H(params, u):
-    """ Better integrand for inverse expansion rate integrals
-    
     Return (1+z)^{-3/2} H0/H(z)
     """
     h = params["H0"] / 100
     Omega_b0 = params['Omega_b_h2'] / h ** 2
     Omega_c0 = Omega_c(params)
-    #Omega_nu_mass = jnp.array([Omega_n_mass(params, uu) for uu in u])
     Omega_gamma = Tcmb_to_Omega_gamma(params["Tcmb"], params["H0"])
-    Omega_nu_mass = Omega_n_mass(params, u)
     Omega_nu_rel = Omega_n_rel(params)
+    Omega_nu_mass = Omega_n_mass(params, u)
     Omega_de0 = Omega_de(params, Omega_nu_rel)
-    return 1. / jnp.sqrt(  # as in CCL or PyCosmo
+    return 1. / jnp.sqrt(
         (Omega_c0 + Omega_b0) +
         Omega_de0 * u ** (-6 * params['w']) +
         (Omega_gamma + Omega_nu_rel + Omega_nu_mass) * u ** (-2) +
         params['Omega_k'] * u ** 2
     )
 
+def dC(params: Dict[str, float], z: jnp.ndarray, nstep: int = 300) -> jnp.ndarray:
+    """Compute the comoving distance at redshift z.
 
-def dC(params, z, nstep=300.):
-    """Return the comoving distance at redshift z
-    
     Distance between comoving object and observer that stay
     constant with time (coordinate).
-
-    For some reason (c was expressed in m/s and H0 in km/s/Mpc in
-    Constants.py) this function return distances in kpc. I you
-    change that, you will have the satisfaction of doing something
-    right, but so many things will break down in my codes that I
-    wont be able to survive.
 
     Parameters:
     -----------
@@ -63,18 +55,15 @@ def dC(params, z, nstep=300.):
     """
     dh = Constants.c / params['H0'] * 1e-3  # Hubble radius in kpc
     u = 1 / jnp.sqrt(1 + z)
-    umin = 0.02  #u.min()
+    umin = 0.02
     step = (1 - umin) / nstep
     _u = jnp.arange(umin + 0.5 * step, 1, step)
     csum = jnp.cumsum(dzoveru3H(params, _u[-1::-1]))[-1::-1]
-    R = linear_interpolation(u, csum, _u - 0.5 * step) \
-        * 2 * step * dh
-    return R
+    return linear_interpolation(u, csum, _u - 0.5 * step) * 2 * step * dh
 
+def dM(params: Dict[str, float], z: jnp.ndarray) -> jnp.ndarray:
+    """Compute the transverse comoving distance.
 
-def dM(params, z):
-    """Return the transverse comoving distance in Mpc.
-    
     The comoving distance between two comoving objects (distant
     galaxies for examples) separated by an angle theta is dM
     theta.
@@ -96,50 +85,27 @@ def dM(params, z):
     branches = (open, flat, close)
     return lax.switch(index, branches, comoving_distance)
 
-
-def dL(params, z):
-    """Luminosity distance in Mpc.
-    """
+def dL(params: Dict[str, float], z: jnp.ndarray) -> jnp.ndarray:
+    """Compute the luminosity distance in Mpc."""
     return (1 + z) * dM(params, z)
-    #return (1 + z) * dC(params, z)
 
+def dA(params: Dict[str, float], z: jnp.ndarray) -> jnp.ndarray:
+    """Compute the angular diameter distance in Mpc.
 
-def dA(params, z):
-    """Angular diameter distance in Mpc.
-    
     The physical proper size of a galaxy which subtend an angle
     theta on the sky is dA * theta
-    
     """
     return dM(params, z) / (1 + z)
 
-
-def mu(params, z):
-    """ Distance modulus
-    """
+def mu(params: Dict[str, float], z: jnp.ndarray) -> jnp.ndarray:
+    """Compute the distance modulus."""
     return 5 * jnp.log10(dL(params, z)) + 25
 
-
-def dVc(params, z):
-    """ Differential comoving volume in Mpc^3
-
-    Notes:
-    ------
-
-    Pretty much equivalent to:
-        1. create a Vc function to compute comoving volume such as:
-         
-            Vc = (4/3) * jnp.pi * dC(params, z)**3
-
-           and computing/evaluating dVc(z) with jax.grad
-
-        2. astropy.cosmology.FlatLambdaCDM.differential_comoving_volume(z).value * 4 * jnp.pi
-
-    """
+def dVc(params: Dict[str, float], z: jnp.ndarray) -> jnp.ndarray:
+    """Calculate the differential comoving volume."""
     dh = Constants.c / params['H0'] * 1e-3
     toto = 1 + z
     return 4 * jnp.pi * (dC(params, z) ** 2) * dh * dzoveru3H(params, 1 / jnp.sqrt(toto)) / (toto ** (3 / 2))
-
 
 
 ###########################
