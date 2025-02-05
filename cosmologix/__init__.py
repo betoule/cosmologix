573bb36767f7a3b3090bbf42332130d4b166c85e
Marc Betoule <marc.betoule@lpnhe.in2p3.fr>
2025-02-05 16:22:59 +0100

    forgotten __init__ file

---
+++ cosmologix/__init__.py
@@ -0,0 +1,15 @@
+from .distances import mu
+
+#
+Planck18 = {
+    "Omega_m": 0.30966,
+    "Tcmb": 2.7255,
+    "Omega_b_h2": 0.0224178,
+    "Omega_k": 0.0,
+    "w": -1.0,
+    "H0": 67.66,
+    "m_nu": 0.06,
+    "Neff": 3.046,
+}
+
+#
