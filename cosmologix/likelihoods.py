ac1f5112976fc2b3a0c6f2b056876ce9c734f4a9
Jérémy Neveu <jeremy.neveu@universite-paris-saclay.fr>
2025-02-21 11:54:54 +0100

    add DESI 2024 YR1 fiducial cosmology

--- cosmologix/likelihoods.py
+++ cosmologix/likelihoods.py
@@ -326,6 +326,24 @@ Planck18 = {
     "m_nu": 0.06,  # jnp.array([0.06, 0.0, 0.0]),
     "Neff": 3.046,
 }
+
+# Fiducial cosmology used in DESI 2024 YR1 BAO measurements
+# Referred as abacus_cosm000 at https://abacussummit.readthedocs.io/en/latest/ cosmologies.html
+# Baseline LCDM, Planck 2018 base_plikHM_TTTEEE_lowl_lowE_lensing mean
+DESI2024YR1_Fiducial = {
+    "Tcmb": 2.7255,  # from Planck18 arxiv:1807.06209 footnote 14 citing Fixsen 2009
+    "Omega_m": (0.02237 + 0.1200) / (67.36 / 100) ** 2,
+    "H0": 67.36,  # ±0.54
+    "Omega_b_h2": 0.02237,  # ±0.00015
+    "Omega_k": 0.0,
+    "w": -1.0,
+    "wa": 0.0,
+    "m_nu": 0.06,  # jnp.array([0.06, 0.0, 0.0]),  # 0.00064420   2.0328
+    "Neff": 3.046,
+
+}
+
+
 # Planck18 = {
 #    "Omega_m": 0.30966,
 #    "Tcmb": 2.7255,
