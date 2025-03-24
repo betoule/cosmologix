d14d1eb20d9e8fbab1a79bed9a2e95cffec59ca5
Marc Betoule <marc.betoule@lpnhe.in2p3.fr>
2025-03-24 12:19:06 +0100

    add corner plots to tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,10 +10,12 @@ def test_main_cli(tmp_path, capsys):
     best_fit_path = tmp_path / "bestfit.pkl"
     contour_path = tmp_path / "contour.pkl"
     plot_path = tmp_path / "plot.png"
+    corner_path = tmp_path / "corner.png"
     test_cases = [
         f"fit -p Planck18 DES-5yr -A -o {best_fit_path.as_posix()}",
         f"explore Omega_m w -p Planck18 DES-5yr -o {contour_path.as_posix()}",
         f"contour {contour_path.as_posix()} -o {plot_path.as_posix()}",
+        f"corner {best_fit_path.as_posix()} --labels 'Planck+DES' -o {plot_path.as_posix()}",
     ]
     for test_case in test_cases:
         with patch("sys.argv", ["cosmologix"] + test_case.split()):
