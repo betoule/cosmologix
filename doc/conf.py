24e9033ae95dfdd2c702987544f2bf1c9a7958db
Maude Le Jeune <lejeune@apc.in2p3.fr>
2025-05-10 09:47:39 +0200

    black

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,7 @@ intersphinc_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 autodoc_type_aliases = {
