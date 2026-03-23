825037bb06b93081e775d0b7b51e00f820c7cd6d
Marc Betoule <marc.betoule@lpnhe.in2p3.fr>
2026-03-23 11:01:21 +0100

    Get rid of insufferable XLA warning

--- cosmologix/__init__.py
+++ cosmologix/__init__.py
@@ -11,6 +11,9 @@ Modules:
     contours: Frequentist confidence contours.
     display: Plotting tools.
 """
+import os
+# Suppress XLA/JAX warnings (adjust level as needed)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 __all__ = [
     "cli",
